4e68dc56c639eb29644853515f9a16bba7608b66
huangjianmei <920287501@qq.com>
2023-12-13 14:11:54 +0800

    列表渲染

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+const list=[
+  {id:1,name:"React"},
+  {id:2,name:"vue"},
+  {id:3,name:"Angular"}
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is App
+      <ul>
+        {
+          list.map(item=><li key="item.id">{item.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
