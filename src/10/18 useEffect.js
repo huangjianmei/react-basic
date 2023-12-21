6a048847dea55ae6ff1e914a1c2823ff9b0d735b
huangjianmei <920287501@qq.com>
2023-12-21 15:54:34 +0800

    useEffect

---
+++ src/10/18 useEffect.js
@@ -0,0 +1,29 @@
+import {useEffect,useState} from "react"
+
+const URL='http://geek.itheima.net/v1_0/channels'
+
+function App() {
+  const [list,setList] = useState([])
+  useEffect(()=>{
+    async function getList(){
+      const res=await fetch(URL)
+      const jsonRes =await res.json()
+      console.log(jsonRes)
+      setList(jsonRes.data.channels)
+    }
+    getList()
+  },[])
+  return(
+    <div>
+      this is app
+      <ul>
+        {
+          list.map(item=><li key="item.id">{item.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+
+export default App;
