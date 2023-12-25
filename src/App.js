f3c4ae6c7803f2723b78c6fea407e6dc98bd31ce
huangjianmei <920287501@qq.com>
2023-12-25 16:36:07 +0800

    安装依赖，添加文件

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-import {useState,createContext,useContext} from "react"
-const msgContext=createContext()
-
-function A(){
-  return (
-    <div>
-      this is A component
-      <B/>
-    </div>
-  )
-}
-function B(){
-  const msg = useContext(msgContext)
-  return (
-    <div>
-      this is B component,,
-      {msg}
-    </div>
-  )
-}
+import {useState} from "react"
 
 function App() {
-  const msg="this is app msg"
-  return(
-    <msgContext.Provider value={msg}>
-      <div>
-        createContext,useContext
-        <hr/>
-        <hr/>
-        this is App
-        <A />
-      </div>
-    </msgContext.Provider>
+  return (
+    <div>this is app</div>
   )
 }
 
